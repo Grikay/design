e358bacfaaaa7d13a3bd03fdad6bb0f96d441270
Grikay <keitgrigoreva@gmail.com>
2023-01-04 18:40:32 +0300

    firstcommit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict"
+// // console.log('Привет Вася'); var - старая врсия let - новая
+
+// message 
+// const quantity = 2
+// // let quantity = 2
+
+// quantity = 4
+ 
+// alert(quantity)
+
+// let firstNum = 5;
+// let secondNum =2;
+
+// console.log(firstNum < secondNum ) //false
+// console.log(firstNum === secondNum ) //для сравнения импользовать всегда тройное ===
+// console.log(firstNum !== secondNum ) // превращает тру в фолс и наоборот 
+
+// let firstNum = 1;
+// let secondNum = 10;
+
+// if(firstNum < secondNum) {
+// console.log(firstNum,'меньше чем', secondNum)
+// }
