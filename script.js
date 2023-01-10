2ad62c2da9e34095075ff509ab8d474addefaa3c
Grikay <keitgrigoreva@gmail.com>
2023-01-10 23:23:14 +0300

    first commit

--- script.js
+++ script.js
@@ -22,3 +22,46 @@
 // if(firstNum < secondNum) {
 // console.log(firstNum,'меньше чем', secondNum)
 // }
+
+let arg1 = prompt('Введи первое число: ');
+if (arg1.length == 0 || arg1.indexOf(' ') != -1) {
+    alert('Некорректный ввод первого числа!');
+}
+// Проверка на NaN
+if (isNaN(Number(arg1))) {
+    alert('Это не число -____-');
+}
+
+let operator = prompt('Введи знак ( +, -, * или / ): ');
+if (operator.length == 0 || operator.indexOf(' ') != -1) {
+    alert('Некорректный ввод знака!');
+}
+// if (operator != '+') {
+//     alert('Некорректный ввод знака!');
+// }
+
+let arg2 = prompt('Введи второе число: ');
+if (arg2.length == 0 || arg2.indexOf(' ') != -1) {
+    alert('Некорректный ввод второго числа!');
+}
+// Проверка на NaN
+if (isNaN(Number(arg2))) {
+    alert('Это не число -____-');
+}
+
+switch(operator) {
+    case '+':  
+        alert(+arg1 + +arg2);
+        break;
+    case '-':
+        alert(+arg1 - +arg2);
+        break;
+    case '*':
+        alert(+arg1 * +arg2);
+        break;
+    case '/':
+        alert(+arg1 / +arg2);
+        break;
+    default:
+        alert('Я не могу это посчитать -______-');
+}
