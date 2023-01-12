7c5bce3c71f1b8ecc526198d8be99ed5872faae5
Grikay <keitgrigoreva@gmail.com>
2023-01-13 01:06:18 +0300

    12

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 "use strict"
-// // console.log('Привет Вася'); var - старая врсия let - новая
-
-// message 
-// const quantity = 2
-// // let quantity = 2
-
-// quantity = 4
- 
-// alert(quantity)
-
-// let firstNum = 5;
-// let secondNum =2;
-
-// console.log(firstNum < secondNum ) //false
-// console.log(firstNum === secondNum ) //для сравнения импользовать всегда тройное ===
-// console.log(firstNum !== secondNum ) // превращает тру в фолс и наоборот 
-
-// let firstNum = 1;
-// let secondNum = 10;
-
-// if(firstNum < secondNum) {
-// console.log(firstNum,'меньше чем', secondNum)
-// }
-
-
 let arg1 = prompt('Введи первое число: ');
 if (arg1.length == 0 || arg1.indexOf(' ') != -1) {
     alert('Некорректный ввод первого числа!');
@@ -41,14 +16,6 @@ if (operator.length == 0 || operator.indexOf(' ') != -1) {
 //     alert('Некорректный ввод знака!');
 // }
 
-let arg2 = prompt('Введи второе число: ');
-if (arg2.length == 0 || arg2.indexOf(' ') != -1) {
-    alert('Некорректный ввод второго числа!');
-}
-// Проверка на NaN
-if (isNaN(Number(arg2))) {
-    alert('Это не число -____-');
-}
 
 switch(operator) {
     case '+':  
