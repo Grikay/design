83ee6d7b1ae5c23330e8be587d0e206be46e953e
Grikay <keitgrigoreva@gmail.com>
2023-01-22 21:43:31 +0300

    F4

---
+++ fib.js
@@ -0,0 +1,17 @@
+"use strict";
+// обьявление глобальных переменных
+let a = 1;
+let b = 1;
+
+function makeFibonacciFunction(number) {
+  for (let i = 3; i <= number; i++) {
+    // i=3, потому что первое и второе значения последовательности заданы a=1, b=1.
+    let c = a + b;
+    a = b; // fibonacci() = fibonacci(i - 2) + fibonacci(i - 1)
+    b = c;
+  }
+  return b;
+}
+
+let tigra = makeFibonacciFunction(5);
+console.log(tigra);
--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 // Объявление директивы
-'use strict'
+// 'use strict'
 
-let arg1;
-do {
-    arg1 = prompt('Введи первое число: ');
-} while(arg1.length === 0 || arg1.indexOf(' ') !== -1 || isNaN(arg1)); 
+// let arg1;
+// do {
+//     arg1 = prompt('Введи первое число: ');
+// } while(arg1.length === 0 || arg1.indexOf(' ') !== -1 || isNaN(arg1));
 
-// arg1.indexOf(' ') != -1 - он ищет подстроку substr в строке str, начиная с позиции pos,
-//  и возвращает позицию, на которой располагается совпадение, либо -1 при отсутствии совпадений
+// // arg1.indexOf(' ') != -1 - он ищет подстроку substr в строке str, начиная с позиции pos,
+// //  и возвращает позицию, на которой располагается совпадение, либо -1 при отсутствии совпадений
 
-const operators = ['+','-','*','/'];
-let operator;
-do {
-    operator = prompt('Введите знак');
-} while(operator.length === 0 || operator.indexOf(' ') !== -1 || !operators.includes(operator));
+// const operators = ['+','-','*','/'];
+// let operator;
+// do {
+//     operator = prompt('Введите знак');
+// } while(operator.length === 0 || operator.indexOf(' ') !== -1 || !operators.includes(operator));
 
-let arg2;
-do {
-    arg2 = prompt('Введи второе число: ');
-} while(arg2.length === 0 || arg2.indexOf(' ') !== -1 || isNaN(arg2));
+// let arg2;
+// do {
+//     arg2 = prompt('Введи второе число: ');
+// } while(arg2.length === 0 || arg2.indexOf(' ') !== -1 || isNaN(arg2));
 
-
-switch(operator) {
-    case '+':  
-        alert(+arg1 + +arg2);
-        break;
-    case '-':
-        alert(+arg1 - +arg2);
-        break;
-    case '*':
-        alert(+arg1 * +arg2);
-        break;
-    case '/':
-        alert(+arg1 / +arg2);
-        break;
-    default:
-        alert('Я не могу это посчитать -______-');
-}
+// switch(operator) {
+//     case '+':
+//         alert(+arg1 + +arg2);
+//         break;
+//     case '-':
+//         alert(+arg1 - +arg2);
+//         break;
+//     case '*':
+//         alert(+arg1 * +arg2);
+//         break;
+//     case '/':
+//         while(arg2 === '0') {
+//             arg2 = prompt('Делить на 0 нельзя. Введите заново второе число');
+//          }
+//         alert(+arg1 / +arg2);
+//         break;
+//     default:
+//         alert('Я не могу это посчитать -______-');
+// }
